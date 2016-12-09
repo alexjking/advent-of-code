2318147210ddcbd0f364966cd5b732200cf1c3a0
Alex King <aking@factset.com>
2016-12-09 21:33:51 +0000

    feat(day 9): part 2 solution worked!
    Updating the part 2 solution with documentation and the answer.
    It turns out that the recursive function where we pass indexes instead of
    strings minimises the memory footprint. This was a very quick solution.

--- src/2016/day_9/part_2.js
+++ src/2016/day_9/part_2.js
@@ -1,6 +1,9 @@
 'use strict';
 
-
+/**
+ * Recursive function to get the length of a string.
+ * We do not copy the string, we just pass indexes around to minimise the memory footprint.
+ */
 function getLength(input, startIndex, endIndex) {
   if (!input || input.length === 0 || startIndex > endIndex) {
     return 0;
@@ -12,11 +15,16 @@ function getLength(input, startIndex, endIndex) {
   while (i < endIndex) {
     const char = input.charAt(i);
 
-    if (char === '') {
+    // if the char is an empty string, just continue
+    if (char === ' ') {
       i++;
       continue;
     }
 
+    // if the current char is a paranthesis, we need to parse this as a whole string and recurse
+    // we firstly find the marker information.
+    // once we have found the marker information, we can recurse on the part of the string that we need
+    // and multiple the length of that string by the times we need to repeat it
     if (char === '(') {
       // progress the pointer to find the whole marker.
       let marker = '';
@@ -55,5 +63,5 @@ function getLength(input, startIndex, endIndex) {
 module.exports = (input) => {
   const sequence = input[0];
 
-  return getLength(sequence, 0, sequence.length);
+  return getLength(sequence, 0, sequence.length); // 11052855125
 }
