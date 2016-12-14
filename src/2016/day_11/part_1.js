f306973da3f99cc600a6e0ff9d890fa0dd42ba1c
Alex King <aking@factset.com>
2016-12-14 15:07:49 +0000

    feat(day11): efficiencies
    Pruning the possible states when we generate them speed sup part 1 by
    double to 15s. Also refactored caching so that we store it in different
    variables to try and improve the speed here. The current implementation
    still doesn't work for part 2. Need to debug to see where the bottlenecks
    are. If memory, we can replace strings in states with a mapping from int
    -> str.

--- src/2016/day_11/part_1.js
+++ src/2016/day_11/part_1.js
@@ -32,14 +32,14 @@ initialState['steps'] = 0;
 // // U = Curium
 // // R = Ruthenium
 // // T = Plutonium
-// let itemsCount = 14;
-// let initialState = {};
-// initialState[0] = ['PG', 'PM', 'EG', 'EM', 'DG', 'DM'];
-// initialState[1] = ['CG', 'UG', 'RG', 'TG' ];
-// initialState[2] = ['CM', 'UM', 'RM', 'TM'];
-// initialState[3] = [];
-// initialState['elevator'] = 0;
-// initialState['steps'] = 0;
+itemsCount = 14;
+initialState = {};
+initialState[0] = ['PG', 'PM', 'EG', 'EM', 'DG', 'DM'];
+initialState[1] = ['CG', 'UG', 'RG', 'TG' ];
+initialState[2] = ['CM', 'UM', 'RM', 'TM'];
+initialState[3] = [];
+initialState['elevator'] = 0;
+initialState['steps'] = 0;
 
 
 
@@ -118,34 +118,133 @@ function getPossibleNextStates(state) {
 }
 
 function isValidFloor(floor) {
-  let generators = floor.reduce((acc, item) => {
-    if (item.charAt(1) === 'G') acc.push(item.charAt(0));
-    return acc;
-  }, []);
+  let generatorMap = {};
+  let generatorCount = false;
+  let machines = [];
 
-  let machines = floor.reduce((acc, item) => {
-    if (item.charAt(1) === 'M') acc.push(item.charAt(0));
-    return acc;
-  }, []);
+  floor.forEach(item => {
+    if (item.charAt(1) === 'G') {
+      generatorMap[item.charAt(0)] = true;
+      generatorCount = true;
+    } else {
+      machines.push(item.charAt(0));
+    }
+  });
 
   // the machines/generators arrays are now just their corresponding letters
   // find out whether we have a machine without a generator
   // as well as another generator (the second gen will fry the chip)
 
   let isInvalid = machines.some((machine) => {
-    if (!generators.includes(machine) && generators.length > 0){
-      return true;
-    } else {
-      return false;
-    }
+    return (generatorMap[machine] !== true) && generatorCount;
   });
 
   return !isInvalid;
 }
 
+// function isValidFloor(floor) {
+//   let generators = floor.reduce((acc, item) => {
+//     if (item.charAt(1) === 'G') acc.push(item.charAt(0));
+//     return acc;
+//   }, []);
+
+//   let machines = floor.reduce((acc, item) => {
+//     if (item.charAt(1) === 'M') acc.push(item.charAt(0));
+//     return acc;
+//   }, []);
+
+//   // the machines/generators arrays are now just their corresponding letters
+//   // find out whether we have a machine without a generator
+//   // as well as another generator (the second gen will fry the chip)
+
+//   let isInvalid = machines.some((machine) => {
+//     if (!generators.includes(machine) && generators.length > 0){
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   return !isInvalid;
+// }
 
 function reachedFinalState(state) {
   return state[3].length === itemsCount;
+}
+
+let cache0 = {};
+let cache1 = {};
+let cache2 = {};
+let cache3 = {};
+
+function serializeState(state) {
+  let serializedString = '';
+  for (let i = 0; i <= 3; i++) {
+    state[i].sort(); // sorting state so that serialization will be the same regardless of order
+    serializedString += 'F' + i;
+    serializedString += state[i].reduce((acc, item) => {
+      return acc + item;
+    }, '');
+  }
+
+  return serializedString;
+}
+function checkCache(state) {
+  let serializedString = serializeState(state);
+
+  switch(state['elevator']) {
+    case 0:
+      if (cache0[serializedString]) return true;
+      break;
+    case 1:
+      if (cache1[serializedString]) return true;
+      break;
+    case 2:
+      if (cache2[serializedString]) return true;
+      break;
+    case 3:
+      if (cache3[serializedString]) return true;
+      break;
+  }
+
+  return false;
+}
+
+function checkAndUpdateCache(state) {
+  let serializedString = serializeState(state);
+
+  switch(state['elevator']) {
+    case 0:
+      if (cache0[serializedString]) {
+        return true;
+      } else {
+        cache0[serializedString] = true;
+      }
+      break;
+    case 1:
+      if (cache1[serializedString]) {
+        return true;
+      } else {
+        cache1[serializedString] = true;
+      }
+      break;
+    case 2:
+      if (cache2[serializedString]) {
+        return true;
+      } else {
+        cache2[serializedString] = true;
+      }
+      break;
+    case 3:
+      if (cache3[serializedString]) {
+        return true;
+      } else {
+        cache3[serializedString] = true;
+      }
+      break;
+  }
+
+  return false;
 }
 
 function serializeState(state) {
@@ -169,29 +268,45 @@ module.exports = (input) => {
   let stateQueue = [];
   stateQueue.push(initialState);
 
+  let i = 0;
+
   while (stateQueue.length > 0) {
     // take the current state.
     let currentState = stateQueue.shift();
 
+    i++;
+    if ((i % 15000) === 0) {
+      console.log('State queue length: ' + stateQueue.length);
+      console.log('Current steps: ' + currentState['steps']);
+      console.log(currentState);
+      console.log('');
+    }
+
 
     // check if we've reached our final state (return early)
     if (reachedFinalState(currentState)) {
+      console.log('FINAL STATE:');
+      console.log(currentState);
       return currentState.steps;
     }
 
     // we have confirmed we're not at the final state.
     // Let's check whether we have encountered this state before
     // continuing early if so, and update the cache if not.
-    let serializedState = serializeState(currentState);
-    if (stateCache[serializedState] === true) {
+    // returns true if it already exists
+    if (checkAndUpdateCache(currentState)) {
       continue;
-    } else {
-      stateCache[serializedState] = true;
     }
 
     // if we haven't reached the final state
     // get all possible next states
     let possibleNextStates = getPossibleNextStates(currentState);
+
+    // pruning possible states before adding them to queue almost halved the run time 15 seconds for part 1.
+    possibleNextStates = possibleNextStates.filter((state) => {
+      return checkCache(state) !== true; // we only want to keep those which don't have cache hits
+    });
+
     stateQueue.push.apply(stateQueue, possibleNextStates);
   }
 }
