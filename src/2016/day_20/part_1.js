14cb42c0a71bbca2781a4b67a1d9be9e89bb9ebf
Alex King <aking@factset.com>
2016-12-20 11:29:14 +0000

    feat(day 20): add solution for part 2
    The part 2 solution was so similar to part 1, so I have just modified the
    part 1 code to print out both the values for part 1 and part 2. Just
    involved continuing the loop until we reach the end of the blacklist.

--- src/2016/day_20/part_1.js
+++ src/2016/day_20/part_1.js
@@ -18,17 +18,41 @@ module.exports = (input) => {
   // loop through the black list, comparing the bounds until we find an element
   // where its lower bound is greater than the maximum upper bound occurred so far
   // the lowest value in this segment is the lowest-valued IP that is not blocked
+
+  // Loop through the black list, comparing the bounds until we find an element
+  // where its lower bound is greater than the maximum upper bound occurred so far
+  // The lowest value in the first one of these segments is the lowest valued IP that
+  // that is not blocked. (Part 1)
+  // We continue looping until we reach the end summing up the total number of IP addresses.
   let i = 0;
   let maxUpper = blackList[i].upper;
+  let validIPAddresses = 0;
   while (i < blackList.length) {
     if (blackList[i].lower > maxUpper + 1) {
-      return maxUpper + 1;  // Part 1 answer: 4793564
+      // print out part 1 answer when necessary
+      if (validIPAddresses === 0) {
+        console.log('Part 1: ', maxUpper + 1); // Part 1 answer: 4793564
+      }
+
+      // add the range to valid IP addresses for part 2
+      validIPAddresses +=  blackList[i].lower - (maxUpper + 1);
     }
 
     // update the max upper and increment our counter
     maxUpper = Math.max(maxUpper, blackList[i].upper)
     i++;
+
+    // break early if the upper bound hits the max int
+    if (maxUpper >= 4294967295) {
+      break;
+    }
   }
 
-  return null;
+  // add any remaining ipaddresses left at the end
+  if (maxUpper < 4294967295) {
+    validIPAddresses += 4294967295 - (maxUpper);
+  }
+
+  console.log('Part 2: ', validIPAddresses);
+  return validIPAddresses; // Part 2: 146
 }
