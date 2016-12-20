863a2d3692f3270b274e1c096cf2cade99c877df
Alex King <aking@factset.com>
2016-12-20 14:40:41 +0000

    feat(day 19): part 2
    Got a working solution to part 2. I have found a pattern for even numbered
    inputs which means we can avoid working the whole thing out properly.
    Explanations in code comments.

--- src/2016/day_19/part_2.js
+++ src/2016/day_19/part_2.js
@@ -1,127 +1,59 @@
 'use strict';
 
-// function getElfIndexForDeletion(elves, currentIndex, invalidElfIndexes) {
-//     // this finds the element on the opposite side of the large circle (including invalid elves)
-//     let halfElves = Math.floor((elves.length) / 2);
-//     let oppositeIndex = (currentIndex + halfElves) % elves.length;
-
-//     // now lets find the number of deleted elements between the currentIndex
-//     // and the opposite index
-//     let numberOfDeletedElves = 0;
-//     if (currentIndex < elves.length / 2) {
-//       numberOfDeletedElves = invalidElfIndexes.reduce((acc, index) => {
-//         return (index > currentIndex && index < oppositeIndex) ? acc + 1 : acc;
-//       }, 0);
-//     } else {
-//       numberOfDeletedElves = invalidElfIndexes.reduce((acc, index) => {
-//         return (index > currentIndex || index < oppositeIndex) ? acc + 1 : acc;
-//       }, 0);
-//     }
-
-//     //let virtualOppositeIndex = (currentIndex + Math.floor((elves.length + invalidElfIndexes.length) / 2)) % elves.length;
-
-//     // get the opposite side of the table.
-//     let invalidElfIncrement = (invalidElfIndexes.length - numberOfDeletedElves) - numberOfDeletedElves;
-//     let virtualOppositeIndex = (currentIndex + Math.floor(elves.length / 2) + invalidElfIncrement) % elves.length;
-
-//     console.log('current index: ', currentIndex, ', oppositeIndex: ', oppositeIndex, ', numberOfDeletedElves: ', numberOfDeletedElves);
-//     console.log('deleting at ', virtualOppositeIndex);
-//     console.log('virtual opposite ', virtualOppositeIndex);
-
-//     return virtualOppositeIndex;
-// }
-
-function getElfIndexForDeletionWhile(elves, currentIndex, invalidElfIndexes) {
-  let halfValidElves = Math.floor((elves.length - invalidElfIndexes.length) / 2);
-
-  //console.log('currentIndex: ', currentIndex);
-
-  // now loop through the indexes incrementing current index until we reach the valid element
-  while (halfValidElves > 0) {
-
-    // increment the current index
-    if (currentIndex < elves.length - 1) {
-      currentIndex++;
-    } else {
-      currentIndex = 0;
-    }
-
-    if (elves[currentIndex] !== -1) {
-      halfValidElves--;
-    }
+function increment(i, elves) {
+  i = (i < elves.length - 1) ?  i + 1 : 0;
+  while (elves[i] === -1) {
+    i = (i < elves.length - 1) ?  i + 1 : 0;
   }
 
- // console.log('deleting at ', currentIndex);
-
-  return currentIndex;
+  return i;
 }
 
-function incrementCurrentIndex(elves, currentIndex) {
-  if (currentIndex < elves.length) {
-    currentIndex++;
-  } else {
-    currentIndex = 0;
-  }
-
-  // loop until we find an elf which is valid
-  while (elves[currentIndex] === -1) {
-    if (currentIndex < elves.length) {
-      currentIndex++;
-    } else {
-      currentIndex = 0;
-    }
-  }
-
-  return currentIndex;
-}
-
+/**
+ * A modified version of the white elephant game. This involves elves stealing from their opposite neighbour.
+ * Which elf gets the last present?
+ *
+ * I noticed that for even inputs, there is a pattern to how elves are removed.
+ * We start on the opposite side of the circle, and loop round deleting the next 2 valid elves
+ * and then skipping a valid elf. This continues until only one elf remains.
+ *
+ * Below contains a simple loop to achieve this, we just have to be slightly careful about incrementing.
+ *
+ * The answer took less than a second to finish, which is better than the previous code which tries to work
+ * out the answer properly (the old code took 17 mins to calculate for 500,000 elves).
+ */
 module.exports = (input) => {
 
-  let numElves = input[0];
+  let numElves = 3012210;
 
+  // initialize the elves
   let elves = [];
-  for (let i = 0; i < 500000; i++) {
+  for (let i = 0; i < numElves; i++) {
     elves[i] = i + 1;
   }
 
-  let currentIndex = 0;
-  let invalidElfIndexes = [];
-  while ((elves.length - invalidElfIndexes.length) > 1) {
-    //console.log('');
-    //console.log(elves, invalidElfIndexes);
-    // lets find which elf to steal from
-    let elfIndexToStealFrom = getElfIndexForDeletionWhile(elves, currentIndex, invalidElfIndexes);
+  let index = Math.floor(numElves / 2);
+  let removedCounter = 0;
+  let skip = 2;
 
-    // steal from the elf
-    elves[elfIndexToStealFrom] = -1;
-    invalidElfIndexes.push(elfIndexToStealFrom);
-
-    // now lets increment to the next available elf
-    currentIndex = incrementCurrentIndex(elves, currentIndex);
-
-    // finally for optimization purposes
-    // check if we have removed half of the elves, and reset the elves array if we ahve
-    if (currentIndex >= elves.length) {
-      console.log('rejiggling elves', currentIndex, elves.length);
-     //console.log(elves);;
-      currentIndex = 0;
-      invalidElfIndexes = [];
-      elves = elves.reduce((acc, elf) => {
-        if (elf !== -1) acc.push(elf);
-        return acc;
-      }, []);
-      //console.log(elves);
-      //console.log('finished rejiggling');
+  while (removedCounter < numElves - 1) {
+    // if skip > 0, mark the current elf as removed and move along
+    if (skip > 0) {
+      elves[index] = -1;
+      removedCounter++;
+      skip--;
+    } else {
+      // else we shouldn't skip, just set the skip count back to 2
+      skip = 2;
     }
+
+    index = increment(index, elves);
   }
 
-  return elves[0];
+  // now loop through the elves until we have one which is alive
+  for (let i = 0; i < numElves; i++) {
+    if (elves[i] > -1) {
+      return elves[i];
+    }
+  }
 }
-
-// Attempt with this code got 10609.
-// Was too low and took an hour.
-// Need to investigate better ways of doing this.
-// 10700 was also too low.
-
-// input 8 should equal 7
-// input 5 should equal 2
