46dcbf4cd66be5adf63ffd0579d1c6b6185ce1a3
Alex King <aking@factset.com>
2016-12-20 13:33:04 +0000

    feat(day 19): part 2
    This is a solution that I believe will work. It is just quite slow, so
    probably won't be suitable for the large input.

--- src/2016/day_19/part_2.js
+++ src/2016/day_19/part_2.js
@@ -1,39 +1,59 @@
 'use strict';
 
-function getElfIndexForDeletion(elves, currentIndex, invalidElfIndexes) {
-    // this finds the element on the opposite side of the large circle (including invalid elves)
-    let halfElves = Math.floor((elves.length) / 2);
-    let oppositeIndex = (currentIndex + halfElves) % elves.length;
+// function getElfIndexForDeletion(elves, currentIndex, invalidElfIndexes) {
+//     // this finds the element on the opposite side of the large circle (including invalid elves)
+//     let halfElves = Math.floor((elves.length) / 2);
+//     let oppositeIndex = (currentIndex + halfElves) % elves.length;
 
-    // now lets find the number of deleted elements between the currentIndex
-    // and the opposite index
-    let numberOfDeletedElves = 0;
-    if (currentIndex < elves.length / 2) {
-      numberOfDeletedElves = invalidElfIndexes.reduce((acc, index) => {
-        return (index > currentIndex && index < oppositeIndex) ? acc + 1 : acc;
-      }, 0);
+//     // now lets find the number of deleted elements between the currentIndex
+//     // and the opposite index
+//     let numberOfDeletedElves = 0;
+//     if (currentIndex < elves.length / 2) {
+//       numberOfDeletedElves = invalidElfIndexes.reduce((acc, index) => {
+//         return (index > currentIndex && index < oppositeIndex) ? acc + 1 : acc;
+//       }, 0);
+//     } else {
+//       numberOfDeletedElves = invalidElfIndexes.reduce((acc, index) => {
+//         return (index > currentIndex || index < oppositeIndex) ? acc + 1 : acc;
+//       }, 0);
+//     }
+
+//     //let virtualOppositeIndex = (currentIndex + Math.floor((elves.length + invalidElfIndexes.length) / 2)) % elves.length;
+
+//     // get the opposite side of the table.
+//     let invalidElfIncrement = (invalidElfIndexes.length - numberOfDeletedElves) - numberOfDeletedElves;
+//     let virtualOppositeIndex = (currentIndex + Math.floor(elves.length / 2) + invalidElfIncrement) % elves.length;
+
+//     console.log('current index: ', currentIndex, ', oppositeIndex: ', oppositeIndex, ', numberOfDeletedElves: ', numberOfDeletedElves);
+//     console.log('deleting at ', virtualOppositeIndex);
+//     console.log('virtual opposite ', virtualOppositeIndex);
+
+//     return virtualOppositeIndex;
+// }
+
+function getElfIndexForDeletionWhile(elves, currentIndex, invalidElfIndexes) {
+  let halfValidElves = Math.floor((elves.length - invalidElfIndexes.length) / 2);
+
+  //console.log('currentIndex: ', currentIndex);
+
+  // now loop through the indexes incrementing current index until we reach the valid element
+  while (halfValidElves > 0) {
+
+    // increment the current index
+    if (currentIndex < elves.length - 1) {
+      currentIndex++;
     } else {
-      numberOfDeletedElves = invalidElfIndexes.reduce((acc, index) => {
-        return (index > currentIndex || index < oppositeIndex) ? acc + 1 : acc;
-      }, 0);
+      currentIndex = 0;
     }
 
-    let virtualOppositeIndex = (currentIndex + Math.floor((elves.length + invalidElfIndexes.length) / 2)) % elves.length;
-
-    // find the index to delete
-    let deletionIndex = oppositeIndex + numberOfDeletedElves - invalidElfIndexes.length;
-
-    if (deletionIndex < 0) {
-      deletionIndex = elves.length + deletionIndex;
+    if (elves[currentIndex] !== -1) {
+      halfValidElves--;
     }
+  }
 
-    deletionIndex = deletionIndex % elves.length;
+ // console.log('deleting at ', currentIndex);
 
-    console.log('current index: ', currentIndex, ', oppositeIndex: ', oppositeIndex, ', numberOfDeletedElves: ', numberOfDeletedElves);
-    console.log('deleting at ', deletionIndex);
-    console.log('virtual opposite ', virtualOppositeIndex);
-
-    return virtualOppositeIndex;
+  return currentIndex;
 }
 
 function incrementCurrentIndex(elves, currentIndex) {
@@ -60,17 +80,17 @@ module.exports = (input) => {
   let numElves = input[0];
 
   let elves = [];
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 500000; i++) {
     elves[i] = i + 1;
   }
 
   let currentIndex = 0;
   let invalidElfIndexes = [];
   while ((elves.length - invalidElfIndexes.length) > 1) {
-    console.log('');
-    console.log(elves, invalidElfIndexes);
+    //console.log('');
+    //console.log(elves, invalidElfIndexes);
     // lets find which elf to steal from
-    let elfIndexToStealFrom = getElfIndexForDeletion(elves, currentIndex, invalidElfIndexes);
+    let elfIndexToStealFrom = getElfIndexForDeletionWhile(elves, currentIndex, invalidElfIndexes);
 
     // steal from the elf
     elves[elfIndexToStealFrom] = -1;
@@ -81,17 +101,17 @@ module.exports = (input) => {
 
     // finally for optimization purposes
     // check if we have removed half of the elves, and reset the elves array if we ahve
-    if (invalidElfIndexes.length >= elves.length / 2) {
-      console.log('rejiggling elves');
-      console.log(elves);
+    if (currentIndex >= elves.length) {
+      console.log('rejiggling elves', currentIndex, elves.length);
+     //console.log(elves);;
       currentIndex = 0;
       invalidElfIndexes = [];
       elves = elves.reduce((acc, elf) => {
         if (elf !== -1) acc.push(elf);
         return acc;
       }, []);
-      console.log(elves);
-      console.log('finished rejiggling');
+      //console.log(elves);
+      //console.log('finished rejiggling');
     }
   }
 
