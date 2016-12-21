40b6d7457d444e290fd683da036fb6b16cfc1ff2
Alex King <aking@factset.com>
2016-12-21 12:46:21 +0000

    feat(day 21): add solution for part 2
    This is very similar to part 1. To descramble, we just need to reverse the
    instructions, and modify any instructions which is not reversible already.

--- src/2016/day_21/part_2.js
+++ src/2016/day_21/part_2.js
@@ -2,12 +2,17 @@
 
 const StringUtils = require('./StringUtils');
 
+// to descramble the password we just need to reverse the input
+// and modify non reversible instructions to their reversed format
+// this is very similar to part 1.
 module.exports = (input) => {
   let utils = new StringUtils();
 
-  let str = 'abcdefgh';
+  // the scrambled string
+  let str = 'fbgdceah';
   let charArr = str.split('');
 
+  input.reverse();
   input.forEach(command => {
     let arr = command.split(' ');
     if (arr[0] === 'swap' && arr[1] === 'position') {
@@ -22,20 +27,20 @@ module.exports = (input) => {
       utils.reversePositions(charArr, x, y);
     } else if (arr[0] === 'rotate' && arr[1] === 'left') {
       let x = arr[2] * 1;
-      utils.rotateLeft(charArr, x);
+      utils.rotateRight(charArr, x); //rotate right instead of left
     } else if (arr[0] === 'rotate' && arr[1] === 'right') {
       let x = arr[2] * 1;
-      utils.rotateRight(charArr, x);
+      utils.rotateLeft(charArr, x); // rotate left instead of right
     } else if (arr[0] === 'move') {
       let x = arr[2] * 1;
       let y = arr[5] * 1;
-      utils.movePosition(charArr, x, y);
+      utils.movePosition(charArr, y, x); // reverse positions (y,x) instead of (x,y)
     } else if (arr[0] == 'rotate' && arr[1] === 'based') {
-      utils.rotateBasedOnLetterPosition(charArr, arr[6]);
+      utils.reverseRotateBasedOnLetterPosition(charArr, arr[6]); // use a reverse letter position function instead
     } else {
       console.error('Invalid Command');
     }
   });
 
-  return charArr.join(''); // Part 1: cbeghdaf
+  return charArr.join(''); // Part 2: bacdefgh
 }
