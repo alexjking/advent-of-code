6671fd708768da15fbd776c60f063d9fa8681c3d
Alex King <aking@factset.com>
2016-12-23 11:48:12 +0000

    chore(day 22): add some ideas for part 2

---
+++ src/2016/day_22/part_2.js
@@ -0,0 +1,8 @@
+// Part 2
+// Move the data from the top right corner to the top left corner.
+// So algorithm as follows:
+//
+// Starting from the top right corner, do a BST.
+// So for all of the Goals neighbours, count how many moves it would take to move them into either of the neighbouring nodes.
+// I should check for how many empty spaces there are. If there is one, that might be key to finding out how to do it all.
+// Looks exceedingly complicated though, so will wait a while for that one!
