6177a4e0167b01f30d7d249631c421c5fcc9ce11
Alex King <alex@alexjking.co.uk>
2017-12-09 12:01:52 +0000

    feat(DAY 8): part 1

---
+++ src/2017/day_8/part_1.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const _ = require('lodash');
+
+const getRegisters = (instructions) => {
+    return _.reduce(instructions, (memo, instruction) => {
+        memo[instruction.register] = 0;
+        return memo;
+    }, {});
+}
+
+const getInstructions = (input) => {
+    return _.map(input, (inputRow) => {
+        inputRow = _.trim(inputRow);
+        let inputArr = inputRow.split(' ');
+        
+        let instruction = {};
+        instruction.register = inputArr[0];
+        
+        let increment = Number(inputArr[2]);
+        if (inputArr[1] === 'dec') {
+            increment = increment * -1;
+        }
+        instruction.increment = increment;
+        
+        instruction.conditionRegister = inputArr[4];
+        instruction.condition = inputArr[5];
+        instruction.conditionValue = Number(inputArr[6]);
+
+        return instruction;
+    });
+};
+
+const passCondition = (instruction, registers) => {
+    let registerValue = registers[instruction.conditionRegister];
+
+    if (_.isUndefined(registerValue)) {
+        console.log('UNDEFINED REGISTEER');
+    }
+
+    switch (instruction.condition) {
+        case '==':
+            return registerValue === instruction.conditionValue;
+        case '>':
+            return registerValue > instruction.conditionValue;
+        case '<':
+            return registerValue < instruction.conditionValue;
+        case '>=':
+            return registerValue >= instruction.conditionValue;
+        case '<=':
+            return registerValue <= instruction.conditionValue;
+        case '!=':
+            return registerValue !== instruction.conditionValue;
+    }
+
+    console.log('ERRORRRR', instruction, registers);
+};
+
+module.exports = (input) => {
+    let instructions = getInstructions(input);
+    let registers = getRegisters(instructions);
+
+    _.forEach(instructions, (instruction) => {
+        if (passCondition(instruction, registers)) {
+            registers[instruction.register] += instruction.increment;
+        }
+    });
+
+    return _.max(_.values(registers));
+};
