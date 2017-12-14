9475c4430d425de6d6465fc1e5d014da7bb69159
Alex King <aking@factset.com>
2017-12-14 11:10:08 +0000

    feat(day 14): part 2
    Added the part 2 solution to the end of part 1.

--- src/2017/day_14/part_1.js
+++ src/2017/day_14/part_1.js
@@ -31,9 +31,60 @@ module.exports = (input) => {
   }
 
   // reduce grid to count the squares
-  return grid.reduce((memo, row) => {
+  let usedSpace = grid.reduce((memo, row) => {
     return memo + row.reduce((rowMemo, bit) => {
       return rowMemo + (bit === '#' ? 1 : 0);
     }, 0);
   }, 0);
+
+  console.log('Part 1:', usedSpace);
+
+  // Modified BFS to calculate number of regions
+  let regions = 0;
+  for (let row = 0; row < 128; row++) {
+    for (let col = 0; col < 128; col++) {
+      // get selected cell
+      let cell = grid[row][col];
+
+      // continue if we have already visited this cell, or it is a free space
+      if (cell === '*' || cell === '.') {
+        continue;
+      }
+
+      // perform BFS to visit whole region, marking cells as visited as we go along
+      regions++;
+      let queue = [{
+        row: row,
+        col: col,
+      }];
+
+      while (queue.length > 0) {
+        let currentCellLocation = queue.shift();
+        let currentCellValue = grid[currentCellLocation.row][currentCellLocation.col];
+        if (currentCellValue === '*' || currentCellValue === '.') {
+          continue;
+        }
+
+        // mark as visited
+        grid[currentCellLocation.row][currentCellLocation.col] = '*';
+
+        // add neighbours to queue
+        if (currentCellLocation.row > 0) {
+          queue.push({row: currentCellLocation.row - 1, col: currentCellLocation.col});
+        }
+        if (currentCellLocation.row < 127) {
+          queue.push({row: currentCellLocation.row + 1, col: currentCellLocation.col});
+        }
+        if (currentCellLocation.col > 0) {
+          queue.push({row: currentCellLocation.row, col: currentCellLocation.col - 1});
+        }
+        if (currentCellLocation.col < 127) {
+          queue.push({row: currentCellLocation.row, col: currentCellLocation.col + 1});
+        }
+      }
+    }
+  }
+
+  console.log('Part 2:', regions);
+  return regions;
 };
