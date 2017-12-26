31c3994e1a8e2a63a6d3e0c96eed1a9fe3be84da
Alex King <aking@factset.com>
2017-12-26 21:52:37 +0000

    feat(day 24): part 1
    It works, but is inefficient. I do lots of string serilzation for the
    components which is incredibly expensive. I should probably just convert
    these to a class, or simple array or two values. TODO later!

---
+++ src/2017/day_24/part_1.js
@@ -0,0 +1,95 @@
+'use strict';
+
+class Bridge {
+  constructor(zeroComponent) {
+    this.zeroComponent = zeroComponent;
+    this.components = [this.zeroComponent];
+
+    let ports = zeroComponent.split('/');
+    this.freePort = (ports[0] === '0') ? ports[1] : ports[0];
+
+    this.blocked = false;
+  }
+
+  canConnect(component) {
+    let ports = component.split('/');
+    let validPort = ports[0] === this.freePort || ports[1] === this.freePort;
+    return validPort && !this.components.includes(component);
+  }
+
+  connect(component) {
+    let ports = component.split('/');
+    this.freePort = (ports[0] === this.freePort) ? ports[1] : ports[0];
+    this.components.push(component);
+  }
+
+  getSum() {
+    return this.components.reduce((memo, comp) => {
+      return memo + comp.split('/').map(x => Number(x)).reduce((sum, x) => sum + x, 0);
+    }, 0);
+  }
+}
+
+module.exports = (input) => {
+
+  let components = input;
+
+  // get the zero ports, the basis for our bridges
+  let zeroComponents = input.filter((component) => {
+    return component.split('/').some(val => val === '0');
+  });
+
+  let currentBridges = zeroComponents.map(zeroComponent => new Bridge(zeroComponent));
+  let finishedBridges = [];
+
+  while (currentBridges.length > 0) {
+    let tempBridges = [];
+
+    // loop through the bridges
+    // - cloning and adding a connector if it is valid
+    // - adding it to the finished bridges array if it is done
+    currentBridges.slice().reverse().forEach((bridge, index) => {
+      let count = 0;
+      components.forEach(component => {
+        if (bridge.canConnect(component)) {
+          let newBridge = new Bridge('x/x');
+          newBridge.zeroComponent = JSON.parse(JSON.stringify(bridge.zeroComponent));
+          newBridge.components = JSON.parse(JSON.stringify(bridge.components));
+          newBridge.freePort = JSON.parse(JSON.stringify(bridge.freePort));
+          newBridge.blocked = JSON.parse(JSON.stringify(bridge.blocked));
+
+          newBridge.connect(component);
+          tempBridges.push(newBridge);
+          count++;
+        }
+      });
+
+      // if there are no new bridges, then we should add the bridge to the
+      // finished bridges.
+      // it has not been aded to the tempBridhes, so it will be removed
+      // from the current bridhes array
+      if (count === 0) {
+        finishedBridges.push(bridge);
+      }
+    });
+
+    currentBridges = tempBridges;
+  }
+
+  // string version for debugging
+  let strArr = finishedBridges.map(bridge => {
+    return bridge.components.join('--');
+  });
+
+  // get the sums for each component
+  let sums = finishedBridges.map(bridge => {
+    return bridge.getSum();
+  });
+
+  // find the bridge with the highest value
+  return sums.reduce((memo, sum) => {
+    return Math.max(memo, sum);
+  }, 0);
+
+  // answer: 1511, 37s
+};
