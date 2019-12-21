fc1ff598ab835503b8e326ec6f36494723fc98a5
Alex King <alexjking@fb.com>
2019-12-21 22:45:12 +0000

    feat(day 4): part 1

---
+++ src/2019/day_4/part_1.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function twoAdjacentDigitsSame(num) {
+  const str = String(num);
+
+  for (let i = 0; i < 5; i++) {
+    if (str.charAt(i) === str.charAt(i + 1)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function ascendingDigits(num) {
+  const a = String(num);
+
+  for (let i = 0; i < 5; i++) {
+    if (Number(a.charAt(i)) > Number(a.charAt(i + 1))) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// 2050
+module.exports = input => {
+  const ranges = input[0].split('-').map(Number);
+
+  let matches = 0;
+  for (let i = ranges[0]; i <= ranges[1]; i++) {
+    if (twoAdjacentDigitsSame(i) && ascendingDigits(i)) {
+      matches++;
+    }
+  }
+
+  return matches;
+}
